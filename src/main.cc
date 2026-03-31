f00ef3fd7df4a181fdc96c915c0e08557a1b017f
Carter Aitken <soupyninja64@gmail.com>
2026-03-31 13:17:18 -0400

    formatting

--- src/main.cc
+++ src/main.cc
@@ -19,31 +19,46 @@
 
 namespace core {
     #define anno(X) do { if constexpr(DEBUG) std::clog << (X); } while(false)
-    template<typename U> constexpr U implicit_cast(typename std::type_identity<U>::type Thing) {
+
+    template<typename U> constexpr U
+    implicit_cast(typename std::type_identity<U>::type Thing) {
         return Thing;
     }
+
     struct r2 {
         double X, Y;
+
         constexpr r2(double X, double Y): X{X}, Y{Y} {}
+
         constexpr r2(std::complex<double> Z) { 
             memcpy(this, &Z, sizeof(*this));
         }
+
         constexpr r2(const r2&) = default;
+
         constexpr r2 &operator=(const core::r2&) = default;
+
         r2 operator+(r2 const&Other) {
             return {X + Other.X, Y + Other.Y};
         }
+
         r2 operator-(r2 const&Other) {
             return {X - Other.X, Y - Other.Y};
         }
+
         r2 operator*(double const&Other) {
             return {X * Other, Y * Other};
         }
+
         operator std::complex<double>() {
             return {X, Y};
         }
+
         #ifdef RAYLIB
-        r2(rl::Vector2 RayVec): X{implicit_cast<double>(RayVec.x)}, Y{implicit_cast<double>(RayVec.y)} {}
+        r2(rl::Vector2 RayVec):
+        X{implicit_cast<double>(RayVec.x)},
+        Y{implicit_cast<double>(RayVec.y)} {}
+
         operator rl::Vector2() {
             return {implicit_cast<float>(X), implicit_cast<float>(Y)};
         }
@@ -56,25 +71,38 @@ namespace core {
 
 namespace screen {
     constexpr const uint64_t Width = 1000;
+
     constexpr const uint64_t Height = 1000;
+
     constexpr const char *const Name = "Julia Plotting";
+
     constinit static double Zoom = 1.;
+
     constinit static core::r2 Center {0.,0.};
+
+    static rl::Color ELECTRIC_AQUA = rl::GetColor(0x51E5FFFF);
+    
+    static rl::Color INDIGO = rl::GetColor(0x440381FF);
+
     static inline uint64_t at(uint64_t X, uint64_t Y) {
         return X + Width * Y;
     }
-    //constexpr static inline rl::Color hex_to_color(int 00000000
-    static rl::Color ELECTRIC_AQUA = rl::GetColor(0x51E5FFFF);
-    static rl::Color INDIGO = rl::GetColor(0x440381FF);
+
 };
 
 using cplx = std::complex<double>;
+
 template<typename type> using func = std::function<type>;
+
 namespace julia {
     static rl::Color JuliaSet[screen::Width*screen::Height];
+
     static rl::Color MandelbrotSet[screen::Width*screen::Height];
+
     static cplx DestinationSet[screen::Width*screen::Height];
+
     constexpr uint64_t N = 100;
+
     static auto Func = [](cplx C) -> func<cplx(cplx)> {
         return [C](cplx Z) -> cplx {
             return std::pow(Z, cplx{2.0}) + C;
@@ -84,25 +112,41 @@ namespace julia {
 
 namespace rl_combat {
     static inline core::r2 screen_to_graph_normal(core::r2 ScrPos) {
-        ScrPos = {ScrPos.X - screen::Width/2, ScrPos.Y - screen::Height/2};
+        using namespace core;
+        using namespace screen;
+        ScrPos = {ScrPos.X - Width/2, ScrPos.Y - Height/2};
         ScrPos.Y *= -1;
-        core::r2 GraphPos = {ScrPos.X * 4.f/core::implicit_cast<double>(screen::Width), ScrPos.Y * 4.f/core::implicit_cast<double>(screen::Height)};
+        r2 GraphPos = {
+            ScrPos.X * 4.f/implicit_cast<double>(Width),
+            ScrPos.Y * 4.f/implicit_cast<double>(Height)
+        };
         return GraphPos;
     }
+
     static inline core::r2 screen_to_graph(core::r2 ScrPos) {
         ScrPos = {ScrPos.X - screen::Width/2, ScrPos.Y - screen::Height/2};
         ScrPos.Y *= -1;
-        core::r2 GraphPos = {ScrPos.X * 4.f/core::implicit_cast<double>(screen::Width), ScrPos.Y * 4.f/core::implicit_cast<double>(screen::Height)};
+        core::r2 GraphPos = {
+            ScrPos.X * 4.f/core::implicit_cast<double>(screen::Width),
+            ScrPos.Y * 4.f/core::implicit_cast<double>(screen::Height)
+        };
         return GraphPos * screen::Zoom - screen::Center;
     }
     static inline core::r2 graph_to_screen(core::r2 GPos) {
         GPos = (GPos+screen::Center) * (1./screen::Zoom);
-        GPos = {GPos.X * core::implicit_cast<double>(screen::Width) /4., GPos.Y *core::implicit_cast<double>(screen::Height)/4.};
+        GPos = {
+            GPos.X * core::implicit_cast<double>(screen::Width) /4.,
+            GPos.Y *core::implicit_cast<double>(screen::Height)/4.
+        };
         GPos = {GPos.X, GPos.Y * -1.};
-        GPos = {GPos.X + core::implicit_cast<double>(screen::Width)/2., GPos.Y + core::implicit_cast<double>(screen::Height) / 2.};
+        GPos = {
+            GPos.X + core::implicit_cast<double>(screen::Width)/2.,
+            GPos.Y + core::implicit_cast<double>(screen::Height)/2.
+        };
         return GPos;
     }
-    static inline rl::Color color_lerp(rl::Color Src, rl::Color Dest, double Reduct) {
+    static inline rl::Color
+    color_lerp(rl::Color Src, rl::Color Dest, double Reduct) {
         rl::Color Color = {0};
         Color.r = Reduct * Dest.r + (1.-Reduct) * Src.r;
         Color.g = Reduct * Dest.g + (1.-Reduct) * Src.g;
@@ -116,9 +160,13 @@ namespace rl_combat {
 };
 
 static std::binary_semaphore ComputingPixelJulia{1};
+
 constinit static std::atomic<uint64_t> Alive = 0;
+
 constinit thread_local bool SplitHorizontally = true;
-constexpr static auto go_compute_julia = [](cplx JuliaConstant, uint64_t DrawingThreadCount) {
+
+constexpr static auto go_compute_julia =
+[](cplx JuliaConstant, uint64_t DrawingThreadCount) -> void {
     using namespace std::placeholders;
     using namespace std::complex_literals;
     using namespace std;
@@ -135,7 +183,8 @@ constexpr static auto go_compute_julia = [](cplx JuliaConstant, uint64_t Drawing
     double EscapeRadius = pow((1. + sqrt(1 + 4.*abs(JuliaConstant)))/2., 2.0);
 
     function<void(uint64_t,uint64_t,uint64_t,uint64_t)> ComputeLine; 
-    ComputeLine = [&,JuliaConstant,JuliaFunc](uint64_t StartX, uint64_t StartY, uint64_t SizeOfChunkX, uint64_t SizeOfChunkY) -> void {
+    ComputeLine = [&,JuliaConstant,JuliaFunc](uint64_t StartX, uint64_t StartY,
+    uint64_t SizeOfChunkX, uint64_t SizeOfChunkY) -> void {
         ComputeRights.acquire();
         uint64_t Work = 0;
         constexpr static uint64_t WorkCapacity = 1 << 21; // defualt == 1 << 21
